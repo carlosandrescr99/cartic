abd0d40f948f2b48856c0cdefebc1883fe1ae90f
Andres.C <carlosandrescr99@gmail.com>
2022-06-27 18:56:59 -0500

    first commit

---
+++ src/main/java/com/conectamosweb/cartic/CarticApplication.java
@@ -0,0 +1,14 @@
+package com.conectamosweb.cartic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarticApplication.class, args);
+                System.out.println("Api run");
+	}
+
+}
---
+++ src/test/java/com/conectamosweb/cartic/CarticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conectamosweb.cartic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
