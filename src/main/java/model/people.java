a2f1c26aa706c253a9149ca7e3f781b3a4bdbbf3
Andres.C <carlosandrescr99@gmail.com>
2022-07-04 23:27:56 -0500

    numbers validation maps

---
+++ src/main/java/model/people.java
@@ -0,0 +1,79 @@
+/*
+ *
+ */
+package model;
+
+/**
+ *
+ * @author ACR-Inc
+ */
+public class people {
+    private String document, documentId, name, lastName, email, phone1, phone2;
+    private int id;
+
+    public String getDocument() {
+        return document;
+    }
+
+    public void setDocument(String document) {
+        this.document = document;
+    }
+
+    public String getDocumentId() {
+        return documentId;
+    }
+
+    public void setDocumentId(String documentId) {
+        this.documentId = documentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone1() {
+        return phone1;
+    }
+
+    public void setPhone1(String phone1) {
+        this.phone1 = phone1;
+    }
+
+    public String getPhone2() {
+        return phone2;
+    }
+
+    public void setPhone2(String phone2) {
+        this.phone2 = phone2;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    
+}
--- src/main/java/utilities/NumbersValidation.java
+++ src/main/java/utilities/NumbersValidation.java
@@ -1,13 +1,105 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * 
+ * 
  */
 package utilities;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 /**
  *
  * @author ACR-Inc
  */
 public class NumbersValidation {
-    
+	// Devolución validación de valores enteros
+	public static int returnInt() {
+		int zInt;
+		Map<Boolean, Integer> mapa = new HashMap<>();
+		mapa = validateInt();
+		if (mapa.containsKey(true)){
+//		      System.out.println(mapa.get(true)); comando para escribir valor de una clave
+		      zInt=(mapa.get(true));
+		    }else {
+		    	zInt=(mapa.get(false));
+			    System.out.println("Solo numeros enteros");
+		    }
+		return zInt;
+	}
+	// Validación de valores enteros
+	public static Map validateInt(){
+		Map<Boolean, Integer> mapa = new HashMap<>();
+        int valueInt;
+        	try{
+                valueInt = new Scanner(System.in).nextInt();
+                mapa.put(true, valueInt);
+                return mapa;
+            }catch(Exception e){
+                valueInt = 0;
+                mapa.put(false, valueInt);
+                return mapa;
+            }
+	}	
+
+
+	// Devolución validación de valores float
+	public static float returnFloat() {
+		float zFloat;
+		Map<Boolean, Float> mapa = new HashMap<>();
+		mapa = validateFloat();
+		if (mapa.containsKey(true)){
+		      System.out.println(mapa.get(true));
+		      zFloat=(mapa.get(true));
+		      System.out.println("Lo encontre");
+		    }else {
+		    	zFloat=(mapa.get(false));
+			    System.out.println("No lo encontre");
+		    }
+		return zFloat;
+	}
+	
+	// Validación de valores Float
+	public static Map validateFloat(){
+		Map<Boolean, Float> mapa = new HashMap<>();
+        float valueFloat;
+        	try{
+                valueFloat = new Scanner(System.in).nextFloat();
+                mapa.put(true, valueFloat);
+                return mapa;
+            }catch(Exception e){
+                valueFloat = 0;
+                mapa.put(false, valueFloat);
+                return mapa;
+            }
+	}
+
+	// Devolución validación de valores double
+	public static double returnDouble() {
+		double zDouble;
+		Map<Boolean, Double> mapa = new HashMap<>();
+		mapa = validateDouble();
+		if (mapa.containsKey(true)){
+
+		      zDouble=(mapa.get(true));
+		    }else {
+		    	zDouble=(mapa.get(false));
+			    System.out.println("Solo numeros validos");
+		    }
+		return zDouble;
+	}
+	
+	// Validación de valores Double
+	public static Map validateDouble(){
+		Map<Boolean, Double> mapa = new HashMap<>();
+		double valueDouble;
+        	try{
+                valueDouble = new Scanner(System.in).nextDouble();
+                mapa.put(true, valueDouble);
+                return mapa;
+            }catch(Exception e){
+                valueDouble = -1;
+                mapa.put(false, valueDouble);
+                return mapa;
+            }
+	}    
 }
